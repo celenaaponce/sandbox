1628bcf29fadaf320b7edbfd1c22d0218d03f170
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-10-25 12:55:10 -0700

    Update ASL2.py

--- pages/ASL2.py
+++ pages/ASL2.py
@@ -85,208 +85,209 @@ def primera_semana():
     
     components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQ4wlJOjhmNap4RDFiDtqNi1cv2PvEsdZnP4ANcRsVCCDgK0NrpYYLfI5BgwVZzlycwNwmvlwU4qnNt/embed?start=false&loop=false&delayms=3000", height=480)
 
+def repaso_general():
+    clms2 = st.columns([1,1])
+    with clms2[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Introducción')
+    with clms2[1]:
+      st.video('https://youtu.be/c1T1CoU0luo')
+    clms26=st.columns([1,1])
+    with clms26[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Cultura')
+    with clms26[1]:
+      st.video('https://youtu.be/t7HzlXNmFJc')
+    clms27=st.columns([1,1])
+    with clms27[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Gramática')
+    with clms27[1]:
+      st.video('https://youtu.be/nBCvy22r6bo')
+    clms28=st.columns([1,1])
+    with clms28[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Cuento (sin subtítulos) 🔇')
+    with clms28[1]:
+      st.video('https://youtu.be/a0s8Zw6T3Fw')  
+    clms29=st.columns([1,1])
+    with clms29[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Cuento (con subtítulos) 🔈')
+    with clms29[1]:
+      st.video('https://youtu.be/GLXz2s5jBAw')    
+    st.divider()
+
+def repaso_lecc1():
+    st.markdown("<h3 style='text-align: center; color: white;'><u>Lección 1</u></h3>", unsafe_allow_html=True)
+    clms3 = st.columns([1,1])
+    with clms3[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Vocabulario y Frases')
+    with clms3[1]:
+      st.video('https://youtu.be/CD9CUAlcRW4')
+    clms4 = st.columns([1,1])
+    with clms4[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Diálogo (sin subtítlos) 🔇')
+    with clms4[1]:
+      st.video('https://youtu.be/a6bMKje9kOY')
+    clms5 = st.columns([1,1])
+    with clms5[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Diálogo (con subtítulos) 🔈')
+    with clms5[1]:
+      st.video('https://youtu.be/fVmnEqqhlpw')
+
+    clms6 = st.columns([1,1])
+    with clms6[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Conversación (sin subtítulos) 🔇')
+    with clms6[1]:
+      st.video('https://youtu.be/tZk1fKC_7hQ')
+    clms7 = st.columns([1,1])
+    with clms7[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Conversación (con subtítulos) 🔈')
+    with clms7[1]:
+      st.video('https://youtu.be/5gYPPZFOO6M')
+
+def repaso_lecc2():
+    st.markdown("<h3 style='text-align: center; color: white;'><u>Lección 2</u></h3>", unsafe_allow_html=True)
+    clms8 = st.columns([1,1])
+    with clms8[0]:
+        st.title('')
+        st.title('')
+        st.subheader('Vocabulario y Frases')
+
+    with clms8[1]:
+        st.video('https://youtu.be/qxd4OUEOCJg')
+    clms9 = st.columns([1,1])
+    with clms9[0]:
+        st.title('')
+        st.title('')
+        st.subheader('Diálogo (sin subtítlos) 🔇')  
+    with clms9[1]:
+        st.video('https://youtu.be/_SDd08h_UEM')
+    clms10=st.columns([1,1])
+    with clms10[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Diálogo (con subtítulos) 🔈')
+    with clms10[1]:
+      st.video('https://youtu.be/KOUtXRQ3kFw')
+    clms11=st.columns([1,1])
+    with clms11[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Conversación (sin subtítulos) 🔇')
+    with clms11[1]:
+      st.video('https://youtu.be/JHFhfpjI3CE')
+    clms12=st.columns([1,1])
+    with clms12[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Conversación (con subtítulos) 🔈')
+    with clms12[1]:
+      st.video('https://youtu.be/x494aKUmEKM')
+
+def repaso_lecc3():
+    st.markdown("<h3 style='text-align: center; color: white;'><u>Lección 3</u></h3>", unsafe_allow_html=True)
+    clms13=st.columns([1,1])
+    with clms13[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Vocabulario')
+    with clms13[1]:
+      st.video('https://youtu.be/_kva3v4OB2E')
+    clms14=st.columns([1,1])
+    with clms14[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Frases')
+    with clms14[1]:
+      st.video('https://youtu.be/IQyhNdrhuR8')
+    clms15=st.columns([1,1])
+    with clms15[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Diálogo (sin subtítlos) 🔇')
+    with clms15[1]:
+      st.video('https://youtu.be/z1EEtsE79-w')
+    clms16=st.columns([1,1])
+    with clms16[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Diálogo (con subtítlos) 🔈')
+    with clms16[1]:
+      st.video('https://youtu.be/5QM2LGdQWvs')
+    clms17=st.columns([1,1])
+    with clms17[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Conversación (sin subtítulos) 🔇')
+    with clms17[1]:
+      st.video('https://youtu.be/8-9YtAezNAE')
+    clms18=st.columns([1,1])
+    with clms18[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Conversación (con subtítulos) 🔈')
+    with clms18[1]:
+      st.video('https://youtu.be/XeXLA8-rWyo')
+    clms19=st.columns([1,1])
+
+def repaso_lecc4():
+    st.markdown("<h3 style='text-align: center; color: white;'><u>Lección 4</u></h3>", unsafe_allow_html=True)
+    clms20=st.columns([1,1])
+    with clms20[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Vocabulario y Frases')
+    with clms20[1]:
+      st.video('https://youtu.be/OZL7UyN6wSw')
+    clms22=st.columns([1,1])
+    with clms22[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Diálogo (sin subtítlos) 🔇')
+    with clms22[1]:
+      st.video('https://youtu.be/beZlTXitb0g')
+    clms23=st.columns([1,1])
+    with clms23[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Diálogo (con subtítlos)')
+    with clms23[1]:
+      st.video('https://youtu.be/ZABG-Zl5qzE?si=JW6JDpHmrYmp_pRt')
+    clms24=st.columns([1,1])
+    with clms24[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Conversación (sin subtítulos) 🔇')
+    with clms24[1]:
+      st.video('https://youtu.be/qktiKSw2kpQ')
+    clms25=st.columns([1,1])
+    with clms25[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Conversación (con subtítulos) 🔈')
+    with clms25[1]:
+      st.video('https://youtu.be/qktiKSw2kpQ')
 def segunda_semana():
     set_styles()
-    tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["Introduccion", "Vocabulario y Frases", "Dialogo sin subtitulos", "Dialogo con subtitulos", "Conversacion sin subtitulos", "Conversacion con subtitulos", "Otro"])
-    with tab1:
-        clms2 = st.columns([1,1])
-        with clms2[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Introducción')
-        with clms2[1]:
-          st.video('https://youtu.be/c1T1CoU0luo')
-    with tab2: 
-        clms3 = st.columns([1,1])
-        with clms3[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Vocabulario y Frases L1')
-        with clms3[1]:
-          st.video('https://youtu.be/CD9CUAlcRW4')
-        clms8 = st.columns([1,1])
-        with clms8[0]:
-            st.title('')
-            st.title('')
-            st.subheader('Vocabulario y Frases L2')
-    
-        with clms8[1]:
-            st.video('https://youtu.be/qxd4OUEOCJg')
-        clms13=st.columns([1,1])
-        with clms13[0]:
-          st.title('')
-          st.title('')
-          st.subheader('VocabularioL3')
-        with clms13[1]:
-          st.video('https://youtu.be/_kva3v4OB2E')
-        clms14=st.columns([1,1])
-        with clms14[0]:
-          st.title('')
-          st.title('')
-          st.subheader('FrasesL3')
-        with clms14[1]:
-          st.video('https://youtu.be/IQyhNdrhuR8')
-        clms20=st.columns([1,1])
-        with clms20[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Vocabulario y FrasesL4')
-        with clms20[1]:
-          st.video('https://youtu.be/OZL7UyN6wSw')
-    with tab3:
-        clms4 = st.columns([1,1])
-        with clms4[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Diálogo (sin subtítlos) 🔇L1')
-        with clms4[1]:
-          st.video('https://youtu.be/a6bMKje9kOY')
-        clms9 = st.columns([1,1])
-        with clms9[0]:
-            st.title('')
-            st.title('')
-            st.subheader('Diálogo (sin subtítlos) 🔇L2')  
-        with clms9[1]:
-            st.video('https://youtu.be/_SDd08h_UEM')
-        clms15=st.columns([1,1])
-        with clms15[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Diálogo (sin subtítlos) 🔇L3')
-        with clms15[1]:
-          st.video('https://youtu.be/z1EEtsE79-w')
-        clms22=st.columns([1,1])
-        with clms22[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Diálogo (sin subtítlos) 🔇L4')
-        with clms22[1]:
-          st.video('https://youtu.be/beZlTXitb0g')
-
-    with tab4:      
-        clms5 = st.columns([1,1])
-        with clms5[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Diálogo (con subtítulos) 🔈L1')
-        with clms5[1]:
-          st.video('https://youtu.be/fVmnEqqhlpw')
-
-        clms10=st.columns([1,1])
-        with clms10[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Diálogo (con subtítulos) 🔈L2')
-        with clms10[1]:
-          st.video('https://youtu.be/KOUtXRQ3kFw')
-        clms16=st.columns([1,1])
-        with clms16[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Diálogo (con subtítlos) 🔈L3')
-        with clms16[1]:
-          st.video('https://youtu.be/5QM2LGdQWvs')
-        clms23=st.columns([1,1])
-        with clms23[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Diálogo (con subtítlos)L4')
-        with clms23[1]:
-          st.video('https://youtu.be/ZABG-Zl5qzE?si=JW6JDpHmrYmp_pRt')
-
-    with tab5:
-        clms6 = st.columns([1,1])
-        with clms6[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Conversación (sin subtítulos) 🔇L1')
-        with clms6[1]:
-          st.video('https://youtu.be/tZk1fKC_7hQ')
-        clms11=st.columns([1,1])
-        with clms11[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Conversación (sin subtítulos) 🔇L2')
-        with clms11[1]:
-          st.video('https://youtu.be/JHFhfpjI3CE')
-        clms17=st.columns([1,1])
-        with clms17[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Conversación (sin subtítulos) 🔇L3')
-        with clms17[1]:
-          st.video('https://youtu.be/8-9YtAezNAE')
-        clms24=st.columns([1,1])
-        with clms24[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Conversación (sin subtítulos) 🔇L4')
-        with clms24[1]:
-          st.video('https://youtu.be/qktiKSw2kpQ')
-
-    with tab6:
-        
-        clms12=st.columns([1,1])
-        with clms12[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Conversación (con subtítulos) 🔈L2')
-        with clms12[1]:
-          st.video('https://youtu.be/x494aKUmEKM')
-
-        clms18=st.columns([1,1])
-        with clms18[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Conversación (con subtítulos) 🔈L3')
-        with clms18[1]:
-          st.video('https://youtu.be/XeXLA8-rWyo')
-
-        clms25=st.columns([1,1])
-        with clms25[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Conversación (con subtítulos) 🔈L4')
-        with clms25[1]:
-          st.video('https://youtu.be/qktiKSw2kpQ')
-        clms7 = st.columns([1,1])
-        with clms7[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Conversación (con subtítulos) 🔈L1')
-        with clms7[1]:
-          st.video('https://youtu.be/5gYPPZFOO6M')
-    with tab7:
-        clms26=st.columns([1,1])
-        with clms26[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Cultura')
-        with clms26[1]:
-          st.video('https://youtu.be/t7HzlXNmFJc')
-        clms27=st.columns([1,1])
-        with clms27[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Gramática')
-        with clms27[1]:
-          st.video('https://youtu.be/nBCvy22r6bo')
-        clms28=st.columns([1,1])
-        with clms28[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Cuento (sin subtítulos) 🔇')
-        with clms28[1]:
-          st.video('https://youtu.be/a0s8Zw6T3Fw')  
-        clms29=st.columns([1,1])
-        with clms29[0]:
-          st.title('')
-          st.title('')
-          st.subheader('Cuento (con subtítulos) 🔈')
-        with clms29[1]:
-          st.video('https://youtu.be/GLXz2s5jBAw')    
-        st.divider()
-
+    st.markdown("<h2 style='text-align: center; color: white;'><u>Videos</u></h2>", unsafe_allow_html=True)
+    st.divider()
     st.markdown("<h4 style='text-align: center; color: white;'><u>Tarea</u></h4>", unsafe_allow_html=True)
     clms30=st.columns([1,1])
     with clms30[0]:
@@ -298,7 +299,6 @@ def segunda_semana():
     st.divider()
     
     components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQ1DBN424b45UbbrNNEHmSvzFqMDfsnLSR2nLK2eS93kdLU3bdQep_btALoKoWxZu0K644csijPRwuP/embed?start=false&loop=false&delayms=3000", height=480)
-
 def segunda_semana_6():
     set_styles()
     st.subheader('Segunda Semana: Colores y Deletrear')
