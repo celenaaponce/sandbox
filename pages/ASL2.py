97e0000d22c440479693041dfc4f71913b658c6e
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-12-09 20:03:08 -0800

    Update ASL2.py

--- pages/ASL2.py
+++ pages/ASL2.py
@@ -20,10 +20,11 @@ Page("pages/10_Entrar.py", "Entrar")
 def set_styles():
     st.write("""
         <style>
-        .tab {
-            display: flex;
-            margin-left: 1em;
-        }
+        a {
+            background-color: #94387f;
+            color: white;
+
+            }
 
         </style>
     """, unsafe_allow_html=True)
@@ -253,9 +254,16 @@ def repaso_lecc4():
     with clms20[0]:
       st.title('')
       st.title('')
-      st.subheader('Vocabulario y Frases')
+      st.subheader('Vocabulario')
     with clms20[1]:
       st.video('https://youtu.be/OZL7UyN6wSw')
+    clms21 = st.columns([1,1])
+    with clms21[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Frases')
+    with clms21[1]:
+      st.video('https://youtu.be/CpQNPG2ZxtQ')
     clms22=st.columns([1,1])
     with clms22[0]:
       st.title('')
@@ -284,10 +292,22 @@ def repaso_lecc4():
       st.subheader('Conversación (con subtítulos) 🔈')
     with clms25[1]:
       st.video('https://youtu.be/qktiKSw2kpQ')
+    st.divider()
+    st.markdown("<h4 style='text-align: center; color: white;'><u>Tarea</u></h4>", unsafe_allow_html=True)
+    clms30=st.columns([1,1])
+    with clms30[0]:
+      st.title('')
+      st.title('')
+      st.subheader('Vocabulario para la semana que viene')
+    with clms30[1]:
+      st.video('https://youtu.be/sVyvzKH_Nsg')  
+    st.divider()
+    
+    components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQ1DBN424b45UbbrNNEHmSvzFqMDfsnLSR2nLK2eS93kdLU3bdQep_btALoKoWxZu0K644csijPRwuP/embed?start=false&loop=false&delayms=3000", height=480)
 
 def colores():
     set_styles()
-    st.subheader('Lección 6: Colores y Deletrear')
+    st.subheader('Lección 6: Colores')
     st.markdown("<h4 style='text-align: center; color: white;'><u>Videos</u></h4>", unsafe_allow_html=True)
     clms21 = st.columns([1,1])
     with clms21[0]:
@@ -344,12 +364,12 @@ def colores():
 
 def deletrear():
     set_styles()
-    st.subheader('Lección 6: Colores y Deletrear')
+    st.subheader('Lección 6: Deletrear')
     st.markdown("<h4 style='text-align: center; color: white;'><u>Videos</u></h4>", unsafe_allow_html=True)
     clms21 = st.columns([1,1])
     with clms21[0]:
         st.title('')
-        st.markdown('<h5>Vocabulario 🔇</h5>', unsafe_allow_html=True)
+        st.markdown('<h5>Vocabulario </h5>', unsafe_allow_html=True)
     with clms21[1]:
         st.video('https://youtu.be/tMqFbxcjx2o')
     clms23 = st.columns([1,1])
@@ -408,10 +428,64 @@ def deletrear():
         st.title('')
         st.markdown('<h5>Practica</h5>', unsafe_allow_html=True)
     with clms28[1]:
-        st.markdown("<a href='https://edpuzzle.com/media/6535ed2547faa53fdf53a7b9' target='_blank'><img style='float: left;' src='https://raw.githubusercontent.com/celenaaponce/sandbox/main/pages/Images/Screenshot%202023-10-22%20at%203.32.28%20PM.png' width='100' height='100'/></a>", unsafe_allow_html=True)     
-        st.markdown("<a href='https://edpuzzle.com/media/6535ed4ea59f3e4030d8a224' target='_blank'><img style='float: left;' src='https://raw.githubusercontent.com/celenaaponce/sandbox/main/pages/Images/Screenshot%202023-10-22%20at%203.32.28%20PM.png' width='100' height='100'/></a>", unsafe_allow_html=True)     
-        st.markdown("<a href='https://edpuzzle.com/media/6535f557ac44ed3fd6871438' target='_blank'><img style='float: left;' src='https://raw.githubusercontent.com/celenaaponce/sandbox/main/pages/Images/Screenshot%202023-10-22%20at%203.32.28%20PM.png' width='100' height='100'/></a>", unsafe_allow_html=True)     
+        st.markdown("<a href='https://edpuzzle.com/media/6561ba3bf53b4c415ee4ec79' target='_blank'><img style='float: left;' src='https://raw.githubusercontent.com/celenaaponce/sandbox/main/pages/Images/Screenshot%202023-10-22%20at%203.32.28%20PM.png' width='100' height='100'/></a>", unsafe_allow_html=True)     
+        st.markdown("<a href='https://edpuzzle.com/media/6561c188f996c941653fc5a0' target='_blank'><img style='float: left;' src='https://raw.githubusercontent.com/celenaaponce/sandbox/main/pages/Images/Screenshot%202023-10-22%20at%203.32.28%20PM.png' width='100' height='100'/></a>", unsafe_allow_html=True)     
 
     st.divider()
             
     components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQXb3L7FrMl7nGhW4MFw_6CT2CKW5MKOBCyI0x9HoNvGuLdhMqcN1fecVAXi2xRkTavM_dwXx1r1RQJ/embed?start=false&loop=false&delayms=3000", height=480)
+
+def escuela1():
+    set_styles()
+    st.subheader('Lección 7: Escuela')
+    st.markdown("<h4 style='text-align: center; color: white;'><u>Videos</u></h4>", unsafe_allow_html=True)
+    clms21 = st.columns([1,1])
+    with clms21[0]:
+        st.title('')
+        st.markdown('<h5>Introduccion </h5>', unsafe_allow_html=True)
+    with clms21[1]:
+        st.video('https://youtu.be/oQQFZSlr-7c')
+    clms23 = st.columns([1,1])
+    with clms23[0]:
+        st.title('')
+        st.markdown('<h5>Repaso y Explicacion</h5>', unsafe_allow_html=True)
+    with clms23[1]:
+        st.video('https://youtu.be/U0I58rdwlDkg')
+    clms24 = st.columns([1,1])
+    with clms24[0]:
+        st.title('')
+        st.markdown('<h5>Conversacion (sin subtitulos) 🔇</h5>', unsafe_allow_html=True)
+    with clms24[1]:    
+        st.video('https://youtu.be/Ey_qhboMTmo')
+    clms25 = st.columns([1,1])
+    with clms25[0]:
+        st.title('')
+        st.markdown('<h5>Conversacion (con subtitulos) 🔈</h5>', unsafe_allow_html=True)
+    with clms25[1]: 
+        st.video('https://youtu.be/qa3gM93D85c')
+    clms26 = st.columns([1,1])
+    with clms26[0]:
+        st.title('')
+        st.markdown('<h5>Cultura Sorda</h5>', unsafe_allow_html=True)
+    with clms26[1]:
+        st.video('https://youtu.be/HhilwM-rUPk')
+
+    st.divider()
+
+    st.markdown("<h4 style='text-align: center; color: white;'><u>Tarea</u></h4>", unsafe_allow_html=True)
+    clms27 = st.columns([1,1])
+    with clms27[0]:
+        st.title('')
+        st.markdown('<h5>Vocabulario para la semana que viene</h5>', unsafe_allow_html=True)
+    with clms27[1]:
+        st.video('https://youtu.be/axnjssoujJQ')
+    clms28 = st.columns([1,1])
+    with clms28[0]:
+        st.title('')
+        st.markdown('<h5>Practica</h5>', unsafe_allow_html=True)
+    with clms28[1]:
+        st.markdown("<a href='https://edpuzzle.com/media/656832c8714d964171966867' target='_blank'><img style='float: left;' src='https://raw.githubusercontent.com/celenaaponce/sandbox/main/pages/Images/Screenshot%202023-10-22%20at%203.32.28%20PM.png' width='100' height='100'/></a>", unsafe_allow_html=True)     
+        st.markdown("<a href='https://edpuzzle.com/media/65683015dc1e1641a09708ad' target='_blank'><img style='float: left;' src='https://raw.githubusercontent.com/celenaaponce/sandbox/main/pages/Images/Screenshot%202023-10-22%20at%203.32.28%20PM.png' width='100' height='100'/></a>", unsafe_allow_html=True)     
+
+    st.divider()
+    components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vTaL-csLK4bnmmbonxNSjLb0lgzB22X8rQahAOtdtEo0zRHKgQ6RTWEBwIOw2yy4DXaI6PauEFEDu78/embed?start=false&loop=false&delayms=3000", height=480)
