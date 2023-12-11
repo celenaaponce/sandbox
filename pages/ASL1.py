a097041cbc2e68a1c6f46085d7c2142b9d516010
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-12-10 16:44:28 -0800

    Update ASL1.py

--- pages/ASL1.py
+++ pages/ASL1.py
@@ -37,7 +37,7 @@ def primera_semana():
 
     set_styles()
     if st.session_state['phone'] == True:
-      size = 75
+      size = 100
     else:
       size = 100
     header = st.session_state['font']
