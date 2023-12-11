c0aeceaa7eff71ff404597fde0e15dba300bcb47
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-12-10 16:25:18 -0800

    Update ASL1.py

--- pages/ASL1.py
+++ pages/ASL1.py
@@ -34,9 +34,9 @@ def primera_semana():
 
     set_styles()
     if st.session_state['phone'] == True:
-      size = 100
+      size = 50
     else:
-      size = 200
+      size = 100
     st.subheader('Primera Semana: Introducción')
     st.markdown("<h4 style='text-align: center; color: white;'><u>Recursos</u></h4>", unsafe_allow_html=True)
     clms = st.columns([1,1])
