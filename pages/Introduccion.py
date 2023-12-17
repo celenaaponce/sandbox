4d575cd7456c1ed99ae6773a22e3dc4160ccbee4
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-12-17 11:37:59 -0800

    test

--- pages/Introduccion.py
+++ pages/Introduccion.py
@@ -32,7 +32,7 @@ def main():
 
     login_sidebar_ASL1()
     authenticator.logout('Salir', 'main')
-    st.write(authenticator.credentials)
+    st.write(authenticator.token)
     st.header("Bienvenido a la clase de ASL 1.")
     st.header("Se puede mirar nuestro curriculo aqui:")
     primera_semana()
