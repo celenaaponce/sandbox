50501929c0ba3a1d2d1046525bdb22fc8dbafc0f
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-12-17 13:13:20 -0800

    pressed

--- auth_custom/streamlit_authenticator/authenticate.py
+++ auth_custom/streamlit_authenticator/authenticate.py
@@ -211,7 +211,7 @@ class Authenticate:
         if location not in ['main', 'sidebar']:
             raise ValueError("Location must be one of 'main' or 'sidebar'")
         if location == 'main':
-            if st.button(button_name, key) or pressed:
+            if pressed:
                 self.cookie_manager.delete(self.cookie_name)
                 st.session_state['logout'] = True
                 st.session_state['name'] = None
