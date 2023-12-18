4a8fa1d06b846c60c2f6087d0b3a14bce8e8c6a5
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-12-18 07:52:10 -0800

    push

--- pages/10_Entrar.py
+++ pages/10_Entrar.py
@@ -116,6 +116,7 @@ def check_password():
         ):
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state['username']
         else:
             st.session_state["password_correct"] = False
 
