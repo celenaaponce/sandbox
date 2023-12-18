4b9eceb110e1aceb06b970122e7d0093221f59cb
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2023-12-18 08:06:03 -0800

    push

--- pages/10_Entrar.py
+++ pages/10_Entrar.py
@@ -108,7 +108,7 @@ def check_password():
         """Checks whether a password entered by the user is correct."""
         st.write('uname', st.session_state['username'])
         st.write('pass', st.session_state['password'])
-        st.write(st.session_state['username'] in st.secrets['passwords'])
+        st.write(st.secrets['passwords'])
         if st.session_state["username"] in st.secrets[
             "passwords"
         ] and hmac.compare_digest(
