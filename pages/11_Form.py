1457b6d375a97c1a24c7b750488d64c6f0dac0e3
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-01-22 21:45:23 -0800

    Update 11_Form.py

--- pages/11_Form.py
+++ pages/11_Form.py
@@ -109,9 +109,9 @@ if __name__ == '__main__':
     img10am = Image.open('10am.jpeg')
     with st.form("Tomar clase de ASL"):
         col1, col2 = st.columns([1, 1])
-        with col1 
+        with col1:
             st.header("¿Como se llama usted?")
-        with col2
+        with col2:
             st.audio('pronunciation_es_nombre.mp3')
         nombre = st.text_input(label = "", placeholder = "Entrar su nombre", label_visibility= "collapsed")
 
