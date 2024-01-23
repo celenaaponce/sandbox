d091b3ab68a69c63f28f49038bb1e53e8c30be9f
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-01-22 22:45:29 -0800

    Update 11_Form.py

--- pages/11_Form.py
+++ pages/11_Form.py
@@ -137,14 +137,14 @@ if __name__ == '__main__':
             st.audio('telefono.m4a')
         telefono = st.text_input(label = "", placeholder = "Entrar su número de teléfono", label_visibility= "collapsed")
 
-        col1, col2, col3= st.columns([.6, .1, .3])
+        col1, col2, col3= st.columns([.7, .1, .2])
         with col1:
             st.header("¿Usted ha tomado clases de lengua de señas antes?")
         with col2:
             st.audio('clases.m4a')
         clases_antes = st.radio(label = "", options = ["Si", "No"], label_visibility= "collapsed")
         
-        col1, col2, col3= st.columns([.3, .1, .6])
+        col1, col2, col3= st.columns([.4, .1, .5])
         with col1:
             st.header("¿Cual temas ya sabe usted?")
         with col2:
