2c89c18cbb8ad36f478da3d05bffcde488c6dd0e
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-01-22 21:52:59 -0800

    Update 11_Form.py

--- pages/11_Form.py
+++ pages/11_Form.py
@@ -112,7 +112,7 @@ if __name__ == '__main__':
     img1pm = Image.open('1pm.jpeg')
     img10am = Image.open('10am.jpeg')
     with st.form("Tomar clase de ASL"):
-        col1, col2 = st.columns([1, .5])
+        col1, col2 = st.columns([1, .2])
         with col1:
             st.header("¿Como se llama usted?")
         with col2:
