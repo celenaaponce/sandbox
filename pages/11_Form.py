eb041b31f3bbd14864914dfdc70678a0eef0322f
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-01-22 23:11:01 -0800

    Update 11_Form.py

--- pages/11_Form.py
+++ pages/11_Form.py
@@ -88,6 +88,10 @@ def ChangeButtonColour(widget_label, font_color, background_color='transparent')
             }}
         }})
     }}
+      function play(){
+       var audio = document.getElementById('audio');
+       audio.play();
+                 }
         </script>
         """
     components.html(f"{htmlstr}", height=0, width=0)
@@ -110,18 +114,11 @@ def send_email(sender, password, receiver, smtp_server, smtp_port, email_message
     text = email_message.as_string()
     server.sendmail(sender, receiver, text)
     server.quit()
-st.markdown("""<body>
-
-    <script>
-  function play(){
-       var audio = document.getElementById('audio');
-       audio.play();
-                 }
-   </script>
+st.markdown("""
 
 <img src='5pm.jpeg' value='PLAY'  onclick='play()'>
 <audio id='audio' src='nombre.m4a' ></audio>
- </body>""", unsafe_allow_html=True)
+ """, unsafe_allow_html=True)
 if __name__ == '__main__':
 
     message = ""
