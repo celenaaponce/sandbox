a8bb49e36800828a238f61519619104aa0192184
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-01-24 13:48:16 -0800

    Update 11_Form.py

--- pages/11_Form.py
+++ pages/11_Form.py
@@ -94,21 +94,22 @@ if __name__ == '__main__':
     img1pm = Image.open('1pm.jpeg')
     img10am = Image.open('10am.jpeg')
         # JavaScript code for playing audio on image click
-    audio_code = f"""
-    <audio id="myAudio" src="{github_audio_url}" height="5"></audio>
-    <script>
-        function playAudio() {{
-            var audio = document.getElementById('myAudio');
-            audio.play();
-        }}
-    </script>
-    """
+
     with st.form("Tomar clase de ASL"):
         col1, col2 = st.columns([.55, .45])
         with col1:
             st.header("¿Como se llama usted?") 
         with col2:
             github_audio_url = "https://raw.githubusercontent.com/celenaaponce/sandbox/main/nombre.m4a"
+            audio_code = f"""
+            <audio id="myAudio" src="{github_audio_url}" height="5"></audio>
+            <script>
+                function playAudio() {{
+                    var audio = document.getElementById('myAudio');
+                    audio.play();
+                }}
+            </script>
+            """
             html(f'<div onclick="playAudio()" style="cursor: pointer;" height="5"><img id="customImage" src="{image_url}" width="50" style="position: absolute; bottom: 0;"/></div>{audio_code}', height=50)
         nombre = st.text_input(label = "", placeholder = "Entrar su nombre", label_visibility= "collapsed")
         col1, col2, col3= st.columns([.5, .2, .3])
