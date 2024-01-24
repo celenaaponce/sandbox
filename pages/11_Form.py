bfc75cf111acd26620ca9d990067ed747f971a0c
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-01-24 12:56:47 -0800

    Update 11_Form.py

--- pages/11_Form.py
+++ pages/11_Form.py
@@ -108,18 +108,23 @@ github_audio_url = "https://raw.githubusercontent.com/celenaaponce/sandbox/main/
 
 # Display the image using raw HTML
 image_url = "https://raw.githubusercontent.com/celenaaponce/sandbox/main/5pm.jpeg"  # Replace with the URL of your image
-st.write(f'<img src="https://raw.githubusercontent.com/celenaaponce/sandbox/main/5pm.jpeg" onclick="playAudio()" />', unsafe_allow_html=True)
+
+st.write(f'<img id="customImage" src="{image_url}" style="cursor: pointer;" />', unsafe_allow_html=True)
 
 # JavaScript code for playing audio on image click
 audio_code = f"""
-    <audio id="myAudio" src="{github_audio_url}"></audio>
-    <script>
-        function playAudio() {{
-            var audio = document.getElementById('myAudio');
+<audio id="myAudio" src="{github_audio_url}"></audio>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {{
+        var image = document.getElementById('customImage');
+        var audio = document.getElementById('myAudio');
+
+        image.addEventListener('click', function() {{
             audio.play();
-        }}
-    </script>
-    """
+        }});
+    }});
+</script>
+"""
 
 st.write(audio_code, unsafe_allow_html=True)
 
