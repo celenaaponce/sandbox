c5d79053a5f67fc2b90c7bb8884ef225eb68171c
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-01-24 10:51:01 -0800

    Update 11_Form.py

--- pages/11_Form.py
+++ pages/11_Form.py
@@ -118,7 +118,7 @@ def play_sound():
     st.title("Audio Player Example")
 
     # Load the audio file
-    audio = AudioSegment.from_file('https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/pronunciation_es_nombre.mp3')
+    audio = AudioSegment.from_file('pronunciation_es_nombre.mp3', 'mp3')
     # Create a button to play/pause
     play_button = st.button("Play")
 
