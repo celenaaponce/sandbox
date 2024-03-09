05fb78a27de3508c0afd1f20338226bbf8c5c1a3
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-08 22:20:37 -0800

    Update Pagina_Principal.py

--- Pagina_Principal.py
+++ Pagina_Principal.py
@@ -2,45 +2,11 @@ import streamlit as st
 from PIL import Image
 import streamlit.components.v1 as components
 from streamlit_extras.switch_page_button import switch_page
-
+from pages.sidebars import regular_sidebar, ChangeButtonColour
 from st_pages import Page, Section,show_pages, add_page_title
 
-show_pages(
-[
-    Page("Pagina_Principal.py", "Pagina Principal"),
-    Page("pages/1_Diccionario.py", "Diccionario"),
-    Page("pages/2_Clases.py", "Clases"),
-    Page("pages/3_Libros.py", "Libros"),
-    Page("pages/4_Recursos.py", "Recursos"),
-    Page("pages/5_Sobre_Yo.py", "Sobre Yo"),
-    Page("pages/6_Diccionario_Completo.py", "Diccionario Completo"),
-    Page("pages/7_Diccionario_por_Letra.py", "Diccionario Por Letra"),
-    Page("pages/8_Diccionario_por_Tema.py", "Diccionario Por Tema"),
-    Page("pages/9_Buscar_Palabra.py", "Buscar Palabra"),
-    Page("pages/10_Entrar.py", "Entrar"),
-    Page("pages/11_Form.py", "Formulario")
-])
-def ChangeButtonColour(widget_label, font_color, background_color='transparent'):
-    htmlstr = f"""
-        <script>
-            var elements = window.parent.document.querySelectorAll('button');
-            for (var i = 0; i < elements.length; ++i) {{ 
-                if (elements[i].innerText == '{widget_label}') {{ 
-                    elements[i].style.color ='{font_color}';
-                    elements[i].style.background = '{background_color}'
-                }}
-            }}
-        function goTo(page) {{
-        page_links = parent.document.querySelectorAll('[data-testid="stSidebarNav"] ul li a')
-        page_links.forEach((link) => {{
-            if (link.text == page) {{
-                link.click()
-            }}
-        }})
-    }}
-        </script>
-        """
-    components.html(f"{htmlstr}", height=0, width=0)
+regular_sidebar()
+
 m = st.markdown("""
 <style>
 div.stButton > button:first-child {
