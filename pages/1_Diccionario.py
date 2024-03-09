9a2b10bb8f49ef60c9671441c995e5566a834807
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-08 20:59:00 -0800

    Update 1_Diccionario.py

--- pages/1_Diccionario.py
+++ pages/1_Diccionario.py
@@ -1,56 +1,11 @@
 import streamlit as st
 from PIL import Image
-import streamlit.components.v1 as components
 from streamlit_extras.switch_page_button import switch_page
-from st_pages import Page, Section,show_pages, add_page_title
+from pages.sidebars import regular_sidebar, ChangeButtonColour, set_styles
+import streamlit.components.v1 as components
 
 st.session_state['password_correct'] = False
-show_pages(
-[
-    Page("Pagina_Principal.py", "Pagina Principal"),
-    Page("pages/1_Diccionario.py", "Diccionario"),
-    Page("pages/2_Clases.py", "Clases"),
-    Page("pages/3_Libros.py", "Libros"),
-    Page("pages/4_Recursos.py", "Recursos"),
-    Page("pages/5_Sobre_Yo.py", "Sobre Yo"),
-    Page("pages/6_Diccionario_Completo.py", "Diccionario Completo"),
-    Page("pages/7_Diccionario_por_Letra.py", "Diccionario Por Letra"),
-    Page("pages/8_Diccionario_por_Tema.py", "Diccionario Por Tema"),
-    Page("pages/9_Buscar_Palabra.py", "Buscar Palabra"),
-    Page("pages/10_Entrar.py", "Entrar"),
-    Page("pages/11_Form.py", "Formulario")
-])
-def set_styles():
-    st.write("""
-        <style>
-        h5 {
-            margin-top: 20px;
-            margin-bottom: 20px;
-        }
-        </style>
-    """, unsafe_allow_html=True)
-
-def ChangeButtonColour(widget_label, font_color, background_color='transparent'):
-    htmlstr = f"""
-        <script>
-            var elements = window.parent.document.querySelectorAll('button');
-            for (var i = 0; i < elements.length; ++i) {{ 
-                if (elements[i].innerText == '{widget_label}') {{ 
-                    elements[i].style.color ='{font_color}';
-                    elements[i].style.background = '{background_color}'
-                }}
-            }}
-        function goTo(page) {{
-        page_links = parent.document.querySelectorAll('[data-testid="stSidebarNav"] ul li a')
-        page_links.forEach((link) => {{
-            if (link.text == page) {{
-                link.click()
-            }}
-        }})
-    }}
-        </script>
-        """
-    components.html(f"{htmlstr}", height=0, width=0)
+regular_sidebar()
 
 m = st.markdown("""
 <style>
@@ -66,10 +21,11 @@ image2 = Image.open('dictionary.png')
 outer_col_2 = st.columns([1, 1])            
 
 with outer_col_2[1]:
-    set_styles()
+    style_html = set_styles('')
+    st.write(style_html, unsafe_allow_html=True)
     st.markdown("<h2 style='text-align: center; color: white;'>Diccionario</h1>", unsafe_allow_html=True)
 
-    st.markdown("<h5 style='text-align: center; color: white;'> Este diccionario es señas en la lengua de ASL, con videos. También tiene fotos, y voz español. </h5>", unsafe_allow_html=True)
+    st.markdown("<h5 style='text-align: center; color: white;'> Este diccionario es señas en la lengua de ASL, con videos. También tiene fotos, y voz en español. </h5>", unsafe_allow_html=True)
 
 with outer_col_2[0]:
     inner_col_2 = st.columns([1, 6, 1])
@@ -80,7 +36,8 @@ outer_col = st.columns([1,1])
 st.divider()
 with outer_col[0]:
     compl = st.button('Diccionario Completo', key='Completo')
-    ChangeButtonColour('Diccionario Completo', '#fffff', '#407bff') 
+    htmlstr = ChangeButtonColour('Diccionario Completo', '#fffff', '#407bff') 
+    components.html(f"{htmlstr}", height=0, width=0)
     if compl:
          switch_page('Diccionario_Completo')
 with outer_col[1]:
@@ -90,17 +47,19 @@ outer_col_3 = st.columns([1,1])
 st.divider()
 with outer_col_3[0]:
     letra = st.button('Buscar por Letra', key='Letra')
-    ChangeButtonColour('Buscar por Letra', '#fffff', '#407bff') 
+    htmlstr = ChangeButtonColour('Buscar por Letra', '#fffff', '#407bff') 
+    components.html(f"{htmlstr}", height=0, width=0)
     if letra:
         switch_page('Diccionario_por_Letra')
 with outer_col_3[1]:
-         st.markdown("<h5 style='text-align: center; color: white;'> Buscar palabras por letra </h5>", unsafe_allow_html=True)
+         st.markdown("<h5 style='text-align: center; color: white;'> Buscar palabras por letra inicial </h5>", unsafe_allow_html=True)
 
 outer_col_4 = st.columns([1,1])
 st.divider()
 with outer_col_4[0]:
     tema = st.button('Buscar por Tema', key='Tema')
-    ChangeButtonColour('Buscar por Tema', '#fffff', '#407bff') 
+    htmlstr = ChangeButtonColour('Buscar por Tema', '#fffff', '#407bff') 
+    components.html(f"{htmlstr}", height=0, width=0)
     if tema:
          switch_page('Diccionario_por_Tema')
 with outer_col_4[1]:
@@ -110,9 +69,9 @@ outer_col_5 = st.columns([1,1])
 st.divider()
 with outer_col_5[0]:
     palabra = st.button('Buscar Palabra', key='Palabra')
-    ChangeButtonColour('Buscar Palabra', '#fffff', '#407bff') 
+    htmlstr = ChangeButtonColour('Buscar Palabra', '#fffff', '#407bff') 
+    components.html(f"{htmlstr}", height=0, width=0)
     if palabra:
          switch_page('Buscar_Palabra')
 with outer_col_5[1]:
-    st.markdown("<h5 style='text-align: center; color: white;'> Buscar palabra por busqueda</h5>", unsafe_allow_html=True)
-
+    st.markdown("<h5 style='text-align: center; color: white;'> Buscar palabra usando búsqueda</h5>", unsafe_allow_html=True)
