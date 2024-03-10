9f4ad09bf587aa3a4dcec11375e07a28c1d41709
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-10 14:48:57 -0700

    Update sidebars.py

--- pages/sidebars.py
+++ pages/sidebars.py
@@ -2,106 +2,80 @@ from st_pages import Page, show_pages
 import streamlit as st
 
 def regular_sidebar():
-
-            show_pages(
-            [
-                Page("Pagina_Principal.py", "Pagina Principal"),
-                Page("pages/1_Diccionario.py", "Diccionario"),
-                Page("pages/2_Clases.py", "Clases"),
-                Page("pages/3_Libros.py", "Libros"),
-                Page("pages/4_Recursos.py", "Recursos"),
-                Page("pages/5_Sobre_Yo.py", "Sobre Yo"),
-                Page("pages/6_Diccionario_Completo.py", "Diccionario Completo"),
-                Page("pages/7_Diccionario_por_Letra.py", "Diccionario Por Letra"),
-                Page("pages/8_Diccionario_por_Tema.py", "Diccionario Por Tema"),
-                Page("pages/9_Buscar_Palabra.py", "Buscar Palabra"),
-                Page("pages/10_Entrar.py", "Entrar"),
-                Page("pages/11_Form.py", "Registrar para Clases")
-            ])
+            st.sidebar.page_link("Pagina_Principal.py", "Pagina Principal")
+            st.sidebar.page_link("pages/1_Diccionario.py", "Diccionario")
+            st.sidebar.page_link("pages/2_Clases.py", "Clases")
+            st.sidebar.page_link("pages/3_Libros.py", "Libros")
+            st.sidebar.page_link("pages/4_Recursos.py", "Recursos")
+            st.sidebar.page_link("pages/5_Sobre_Yo.py", "Sobre Yo")
+            st.sidebar.page_link("pages/6_Diccionario_Completo.py", "Diccionario Completo")
+            st.sidebar.page_link("pages/7_Diccionario_por_Letra.py", "Diccionario Por Letra")
+            st.sidebar.page_link("pages/8_Diccionario_por_Tema.py", "Diccionario Por Tema")
+            st.sidebar.page_link("pages/9_Buscar_Palabra.py", "Buscar Palabra")
+            st.sidebar.page_link("pages/10_Entrar.py", "Entrar")
                 
 def login_sidebar_ASL1():
-        if st.session_state['option'] == "ASL 1":
-                show_pages(
-            [
-                Page("Pagina_Principal.py", "Pagina Principal"),
-                Page("pages/1_Diccionario.py", "Diccionario"),
-                Page("pages/2_Clases.py", "Clases"),
-                Page("pages/3_Libros.py", "Libros"),
-                Page("pages/4_Recursos.py", "Recursos"),
-                Page("pages/5_Sobre_Yo.py", "Sobre Yo"),
-                Page("pages/6_Diccionario_Completo.py", "Diccionario Completo"),
-                Page("pages/7_Diccionario_por_Letra.py", "Diccionario Por Letra"),
-                Page("pages/8_Diccionario_por_Tema.py", "Diccionario Por Tema"),
-                Page("pages/9_Buscar_Palabra.py", "Buscar Palabra"),
-                Page("pages/11_Form.py", "Registrar para Clases"),
-                Page("pages/10_Entrar.py", "Entrar"),
-                Page("pages/Introduccion_a_ASL_1.py", "Introducción a ASL 1"),
-                Page("pages/Bravo_1.py", "Conocer la Familia Bravo"),
-                Page("pages/holidays_spring.py", "Días Festivos")
-            ]
-        )
+            st.sidebar.page_link("Pagina_Principal.py", "Pagina Principal")
+            st.sidebar.page_link("pages/1_Diccionario.py", "Diccionario")
+            st.sidebar.page_link("pages/2_Clases.py", "Clases")
+            st.sidebar.page_link("pages/3_Libros.py", "Libros")
+            st.sidebar.page_link("pages/4_Recursos.py", "Recursos")
+            st.sidebar.page_link("pages/5_Sobre_Yo.py", "Sobre Yo")
+            st.sidebar.page_link("pages/6_Diccionario_Completo.py", "Diccionario Completo")
+            st.sidebar.page_link("pages/7_Diccionario_por_Letra.py", "Diccionario Por Letra")
+            st.sidebar.page_link("pages/8_Diccionario_por_Tema.py", "Diccionario Por Tema")
+            st.sidebar.page_link("pages/9_Buscar_Palabra.py", "Buscar Palabra")
+            st.sidebar.page_link("pages/10_Entrar.py", "Entrar")
+            st.sidebar.page_link("pages/Introduccion_a_ASL_1.py", "Introducción a ASL 1")
+            st.sidebar.page_link("pages/Bravo_1.py", "Conocer la Familia Bravo")
+            st.sidebar.page_link("pages/holidays_spring.py", "Días Festivos")
         
 def login_sidebar_ASL2():
-        show_pages(
-    [
-        Page("Pagina_Principal.py", "Pagina Principal"),
-        Page("pages/1_Diccionario.py", "Diccionario"),
-        Page("pages/2_Clases.py", "Clases"),
-        Page("pages/3_Libros.py", "Libros"),
-        Page("pages/4_Recursos.py", "Recursos"),
-        Page("pages/5_Sobre_Yo.py", "Sobre Yo"),
-        Page("pages/6_Diccionario_Completo.py", "Diccionario Completo"),
-        Page("pages/7_Diccionario_por_Letra.py", "Diccionario Por Letra"),
-        Page("pages/8_Diccionario_por_Tema.py", "Diccionario Por Tema"),
-        Page("pages/9_Buscar_Palabra.py", "Buscar Palabra"),
-        Page("pages/11_Form.py", "Registrar para Clases"),
-        Page("pages/10_Entrar.py", "Entrar"),
-        Page("pages/Introduccion_a_ASL_2.py", "Introducción a ASL 2"),
-        Page("pages/Bravo_4.py", "Ir de Compras"),
-        Page("pages/holidays_spring_2.py", "Días Festivos")
-    ]
-)
+            st.sidebar.page_link("Pagina_Principal.py", "Pagina Principal")
+            st.sidebar.page_link("pages/1_Diccionario.py", "Diccionario")
+            st.sidebar.page_link("pages/2_Clases.py", "Clases")
+            st.sidebar.page_link("pages/3_Libros.py", "Libros")
+            st.sidebar.page_link("pages/4_Recursos.py", "Recursos")
+            st.sidebar.page_link("pages/5_Sobre_Yo.py", "Sobre Yo")
+            st.sidebar.page_link("pages/6_Diccionario_Completo.py", "Diccionario Completo")
+            st.sidebar.page_link("pages/7_Diccionario_por_Letra.py", "Diccionario Por Letra")
+            st.sidebar.page_link("pages/8_Diccionario_por_Tema.py", "Diccionario Por Tema")
+            st.sidebar.page_link("pages/9_Buscar_Palabra.py", "Buscar Palabra")
+            st.sidebar.page_link("pages/10_Entrar.py", "Entrar")
+            st.sidebar.page_link("pages/Introduccion_a_ASL_2.py", "Introducción a ASL 2")
+            st.sidebar.page_link("pages/Bravo_4.py", "Ir de Compras")
+            st.sidebar.page_link("pages/holidays_spring_2.py", "Días Festivos")
 
 def login_sidebar_ASL3():
-        show_pages(
-    [
-        Page("Pagina_Principal.py", "Pagina Principal"),
-        Page("pages/1_Diccionario.py", "Diccionario"),
-        Page("pages/2_Clases.py", "Clases"),
-        Page("pages/3_Libros.py", "Libros"),
-        Page("pages/4_Recursos.py", "Recursos"),
-        Page("pages/5_Sobre_Yo.py", "Sobre Yo"),
-        Page("pages/6_Diccionario_Completo.py", "Diccionario Completo"),
-        Page("pages/7_Diccionario_por_Letra.py", "Diccionario Por Letra"),
-        Page("pages/8_Diccionario_por_Tema.py", "Diccionario Por Tema"),
-        Page("pages/9_Buscar_Palabra.py", "Buscar Palabra"),
-        Page("pages/11_Form.py", "Registrar para Clases"),
-        Page("pages/10_Entrar.py", "Entrar"),
-        Page("pages/Introduccion_a_ASL_3.py", "Introducción a ASL 3"),
-        Page("pages/Bravo_7.py", "Escuela"),
-        Page("pages/holidays_spring_3.py", "Días Festivos")
-    ]
-)
+            st.sidebar.page_link("Pagina_Principal.py", "Pagina Principal")
+            st.sidebar.page_link("pages/1_Diccionario.py", "Diccionario")
+            st.sidebar.page_link("pages/2_Clases.py", "Clases")
+            st.sidebar.page_link("pages/3_Libros.py", "Libros")
+            st.sidebar.page_link("pages/4_Recursos.py", "Recursos")
+            st.sidebar.page_link("pages/5_Sobre_Yo.py", "Sobre Yo")
+            st.sidebar.page_link("pages/6_Diccionario_Completo.py", "Diccionario Completo")
+            st.sidebar.page_link("pages/7_Diccionario_por_Letra.py", "Diccionario Por Letra")
+            st.sidebar.page_link("pages/8_Diccionario_por_Tema.py", "Diccionario Por Tema")
+            st.sidebar.page_link("pages/9_Buscar_Palabra.py", "Buscar Palabra")
+            st.sidebar.page_link("pages/10_Entrar.py", "Entrar")
+            st.sidebar.page_link("pages/Introduccion_a_ASL_3.py", "Introducción a ASL 3")
+            st.sidebar.page_link("pages/Bravo_7.py", "Escuela")
+            st.sidebar.page_link("pages/holidays_spring_3.py", "Días Festivos")
         
 def login_sidebar_ASLAtHome2():
-        show_pages(
-    [
-        Page("Pagina_Principal.py", "Pagina Principal"),
-        Page("pages/1_Diccionario.py", "Diccionario"),
-        Page("pages/2_Clases.py", "Clases"),
-        Page("pages/3_Libros.py", "Libros"),
-        Page("pages/4_Recursos.py", "Recursos"),
-        Page("pages/5_Sobre_Yo.py", "Sobre Yo"),
-        Page("pages/6_Diccionario_Completo.py", "Diccionario Completo"),
-        Page("pages/7_Diccionario_por_Letra.py", "Diccionario Por Letra"),
-        Page("pages/8_Diccionario_por_Tema.py", "Diccionario Por Tema"),
-        Page("pages/9_Buscar_Palabra.py", "Buscar Palabra"),
-        Page("pages/11_Form.py", "Registrar para Clases"),
-        Page("pages/10_Entrar.py", "Entrar"),
-        Page("pages/Introduccion_a_ASL_En_Casa.py"),
-        Page("pages/holidays_spring_aah.py", "Días Festivos")
-    ]
-)
+            st.sidebar.page_link("Pagina_Principal.py", "Pagina Principal")
+            st.sidebar.page_link("pages/1_Diccionario.py", "Diccionario")
+            st.sidebar.page_link("pages/2_Clases.py", "Clases")
+            st.sidebar.page_link("pages/3_Libros.py", "Libros")
+            st.sidebar.page_link("pages/4_Recursos.py", "Recursos")
+            st.sidebar.page_link("pages/5_Sobre_Yo.py", "Sobre Yo")
+            st.sidebar.page_link("pages/6_Diccionario_Completo.py", "Diccionario Completo")
+            st.sidebar.page_link("pages/7_Diccionario_por_Letra.py", "Diccionario Por Letra")
+            st.sidebar.page_link("pages/8_Diccionario_por_Tema.py", "Diccionario Por Tema")
+            st.sidebar.page_link("pages/9_Buscar_Palabra.py", "Buscar Palabra")
+            st.sidebar.page_link("pages/10_Entrar.py", "Entrar")
+            st.sidebar.page_link("pages/Introduccion_a_ASL_En_Casa.py")
+            st.sidebar.page_link("pages/holidays_spring_aah.py", "Días Festivos")
         
 def ChangeButtonColour(widget_label, font_color, background_color='transparent'):
     htmlstr = f"""
