e30b788c068b2ae7c49901957f2cb0308e8333fa
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-11 10:46:12 -0700

    Update Diccionario.py

--- pages/Diccionario.py
+++ pages/Diccionario.py
@@ -9,14 +9,8 @@ from modules.nav import MenuButtons
 from pages.account import get_roles
 
 
-# If user refreshes the page, go to the login page because
-# in there we have the facility to check the login status.
-if 'authentication_status' not in ss:
-    st.switch_page('./pages/account.py')
 
-MenuButtons(get_roles())
-st.session_state['password_correct'] = False
-# regular_sidebar()
+regular_sidebar()
 
 m = st.markdown("""
 <style>
