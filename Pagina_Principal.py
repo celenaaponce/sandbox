ded4bff5080a3d39ab095a4a2ecce9c62daeaca2
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-11 09:42:45 -0700

    Update Pagina_Principal.py

--- Pagina_Principal.py
+++ Pagina_Principal.py
@@ -52,8 +52,8 @@ with cont_1:
     with outer_cols[1]:
         inner_cols = st.columns([1, 6, 1])
         with inner_cols[1]:
-            # st.image('./web_img/Online world-cuate (2).png')
-            components.html("""<img src="https://raw.githubusercontent.com/celenaaponce/sandbox/main/web_img/Online%20world-cuate%20(2).png" alt="Girl in a jacket">""", height=400, width=500)
+            st.image('https://raw.githubusercontent.com/celenaaponce/sandbox/main/web_img/Online%20world-cuate%20(2).png')
+            # components.html("""<img src="https://raw.githubusercontent.com/celenaaponce/sandbox/main/web_img/Online%20world-cuate%20(2).png" alt="Girl in a jacket">""", height=400, width=500)
         quien = st.button('Quien Soy', key='Quien')
         htmlstr = ChangeButtonColour('Quien Soy', '#fffff', '#94387f') 
         components.html(f"{htmlstr}", height=0, width=0)
