8f3ae95a573570aa80f061aeb09a52b46652ee7d
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-11 16:22:05 -0700

    Update Entrar.py

--- pages/Entrar.py
+++ pages/Entrar.py
@@ -58,37 +58,37 @@ MenuButtons(get_roles())
 # else:       
 #     classoption = ""
 #     check_password()
-if 'password_correct' not in st.session_state:
-    st.session_state.password_correct = False
-def password_entered():
-        """Checks whether a password entered by the user is correct."""
-        if st.session_state['password'] == st.secrets['password']:
-                st.session_state["password_correct"] = True
-        else:
-                st.session_state["password_correct"] = False
-with st.form("Credentials"):
+# if 'password_correct' not in st.session_state:
+#     st.session_state.password_correct = False
+# def password_entered():
+#         """Checks whether a password entered by the user is correct."""
+#         if st.session_state['password'] == st.secrets['password']:
+#                 st.session_state["password_correct"] = True
+#         else:
+#                 st.session_state["password_correct"] = False
+# with st.form("Credentials"):
 
-    option = st.selectbox(
-            '¿Cual clase?',
-            ('ASL 1', 'ASL 2', 'ASL 3', 'ASL En Casa'), key='option')
-    st.text_input("Contraseña", type="password", key="password")
-    st.form_submit_button("Entrar", on_click=password_entered)
-if st.session_state.password_correct == True:    
-    if st.session_state['option'] == 'ASL 1':
-        login_sidebar_ASL1()
-        switch_page("Introducción_a_ASL_1")
+#     option = st.selectbox(
+#             '¿Cual clase?',
+#             ('ASL 1', 'ASL 2', 'ASL 3', 'ASL En Casa'), key='option')
+#     st.text_input("Contraseña", type="password", key="password")
+#     st.form_submit_button("Entrar", on_click=password_entered)
+# if st.session_state.password_correct == True:    
+#     if st.session_state['option'] == 'ASL 1':
+#         login_sidebar_ASL1()
+#         switch_page("Introducción_a_ASL_1")
     
-    elif st.session_state['option'] == 'ASL 2':
-        login_sidebar_ASL2()
-        switch_page("Introducción_a_ASL_2")
+#     elif st.session_state['option'] == 'ASL 2':
+#         login_sidebar_ASL2()
+#         switch_page("Introducción_a_ASL_2")
         
-    elif st.session_state['option'] == 'ASL En Casa':
-        login_sidebar_ASLAtHome2()
-        switch_page("Introduccion_a_ASL_En_Casa")
+#     elif st.session_state['option'] == 'ASL En Casa':
+#         login_sidebar_ASLAtHome2()
+#         switch_page("Introduccion_a_ASL_En_Casa")
     
-    elif st.session_state['option'] == 'ASL 3':
-        login_sidebar_ASL3()
-        switch_page("Introducción_a_ASL_3")
-else:
+#     elif st.session_state['option'] == 'ASL 3':
+#         login_sidebar_ASL3()
+#         switch_page("Introducción_a_ASL_3")
+# else:
 
-    st.write('wrong password')
+#     st.write('wrong password')
