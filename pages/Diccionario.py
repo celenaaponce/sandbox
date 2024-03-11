5490c235ae52c31b99b0a5551b8f4fa72b7fce58
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-10 23:02:27 -0700

    Update Diccionario.py

--- pages/Diccionario.py
+++ pages/Diccionario.py
@@ -3,9 +3,20 @@ from PIL import Image
 from streamlit_extras.switch_page_button import switch_page
 from pages.sidebars import regular_sidebar, ChangeButtonColour, set_styles
 import streamlit.components.v1 as components
+import streamlit as st
+from streamlit import session_state as ss
+from modules.nav import MenuButtons
+from pages.account import get_roles
 
+
+# If user refreshes the page, go to the login page because
+# in there we have the facility to check the login status.
+if 'authentication_status' not in ss:
+    st.switch_page('./pages/account.py')
+
+MenuButtons(get_roles())
 st.session_state['password_correct'] = False
-regular_sidebar()
+# regular_sidebar()
 
 m = st.markdown("""
 <style>
