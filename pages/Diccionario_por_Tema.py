17c177d7159c524b36943d76a95272dd84efacf5
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-11 10:47:21 -0700

    Update Diccionario_por_Tema.py

--- pages/Diccionario_por_Tema.py
+++ pages/Diccionario_por_Tema.py
@@ -2,20 +2,8 @@ import streamlit as st
 import pandas as pd
 from pages.sidebars import regular_sidebar
 from st_click_detector import click_detector
-import streamlit as st
-from streamlit import session_state as ss
-from modules.nav import MenuButtons
-from pages.account import get_roles
 
-
-# If user refreshes the page, go to the login page because
-# in there we have the facility to check the login status.
-if 'authentication_status' not in ss:
-    st.switch_page('./pages/account.py')
-
-MenuButtons(get_roles())
-st.session_state['password_correct'] = False
-# regular_sidebar()
+regular_sidebar()
 st.set_page_config(layout="wide", page_title="Diccionario Por Tema")
 increment = None
 reset1 = False
