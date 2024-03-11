c3bebc1736d59d7b6394ea1e236b322012ea284b
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-11 16:31:22 -0700

    Update Entrar.py

--- pages/Entrar.py
+++ pages/Entrar.py
@@ -15,51 +15,6 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-# If user refreshes the page, go to the login page because
-# in there we have the facility to check the login status.
-if 'authentication_status' not in ss:
-    st.switch_page('./pages/account.py')
-
-MenuButtons(get_roles())
-# regular_sidebar()
-# def check_password():
-#     """Returns `True` if the user had a correct password."""
-
-#     def login_form():
-#         """Form with widgets to collect user information"""
-#         with st.form("Credentials"):
-
-#             option = st.selectbox(
-#                     '¿Cual clase?',
-#                     ('ASL 1', 'ASL 2', 'ASL 3', 'ASL En Casa'), key='option')
-#             st.text_input("Contraseña", type="password", key="password")
-#             st.form_submit_button("Entrar", on_click=password_entered)
-
-#     def password_entered():
-#         """Checks whether a password entered by the user is correct."""
-#         if st.session_state['password'] == st.secrets['password']:
-#                 st.session_state["password_correct"] = True
-#         else:
-#                 st.session_state["password_correct"] = False
-#     # Return True if the username + password is validated.
-#     if st.session_state.get("password_correct", False):
-#         return True
-
-#     # Show inputs for username + password.
-#     login_form()
-
-#     return False
-
-# if not check_password():
-#     st.stop()
-
-# if 'option' in st.session_state.keys():
-#     classoption = st.session_state['option']
-# else:       
-#     classoption = ""
-#     check_password()
-# if 'password_correct' not in st.session_state:
-#     st.session_state.password_correct = False
 def password_entered():
         """Checks whether a password entered by the user is correct."""
         if st.session_state['password'] == st.secrets['password']:
@@ -75,7 +30,7 @@ with st.form("Credentials"):
     st.form_submit_button("Entrar", on_click=password_entered)
 if st.session_state.password_correct == True:    
     if st.session_state['option'] == 'ASL 1':
-        login_sidebar_ASL1()
+        MenuButtons(get_roles())
         switch_page("Introducción_a_ASL_1")
     
     elif st.session_state['option'] == 'ASL 2':
