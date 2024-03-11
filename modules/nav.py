563d1b9b9133ba13dddc354175ec6244e1746245
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-11 16:46:49 -0700

    Update nav.py

--- modules/nav.py
+++ modules/nav.py
@@ -28,7 +28,7 @@ def ASL1_sidebar():
             st.sidebar.page_link("pages/Diccionario_por_Tema.py", label="Diccionario Por Tema")
             st.sidebar.page_link("pages/Buscar_Palabra.py", label="Buscar Palabra")
             st.sidebar.page_link("pages/Entrar.py", label="Entrar")
-            st.sidebar.page_link("pages/helper/Introduccion_a_ASL_1.py", label="Introducción a ASL 1")
+            st.sidebar.page_link("pages/Introduccion_a_ASL_1.py", label="Introducción a ASL 1")
             st.sidebar.page_link("pages/helper/Bravo_1.py", label="Conocer la Familia Bravo")
             st.sidebar.page_link("pages/helper/holidays_spring.py", label="Días Festivos")
             st.switch_page('pages/helper/Introduccion_a_ASL_1.py')
