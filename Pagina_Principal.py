7fc00cc5a66168a5e30418598af2c17593a9cf49
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-11 09:12:25 -0700

    Update Pagina_Principal.py

--- Pagina_Principal.py
+++ Pagina_Principal.py
@@ -14,22 +14,6 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-# If user refreshes the page, go to the login page because
-# in there we have the facility to check the login status.
-# if 'authentication_status' not in ss:
-#     st.switch_page('./pages/account.py')
-
-
-# MenuButtons(get_roles())
-# st.header('Home page')
-
-
-# # Protected content in home page.
-# if ss.authentication_status:
-#     st.write('This content is only accessible for logged in users.')
-# else:
-#     st.write('Please log in on login page.')
-
 regular_sidebar()
 
 m = st.markdown("""
