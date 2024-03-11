36dfd05c94a8116436d7302e0869a6ecc9a57381
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-10 23:02:41 -0700

    Update Diccionario_Completo.py

--- pages/Diccionario_Completo.py
+++ pages/Diccionario_Completo.py
@@ -5,7 +5,19 @@ import pandas as pd
 from pathlib import Path 
 from pages.sidebars import regular_sidebar
 st.session_state['password_correct'] = False
-regular_sidebar()
+import streamlit as st
+from streamlit import session_state as ss
+from modules.nav import MenuButtons
+from pages.account import get_roles
+
+
+# If user refreshes the page, go to the login page because
+# in there we have the facility to check the login status.
+if 'authentication_status' not in ss:
+    st.switch_page('./pages/account.py')
+
+MenuButtons(get_roles())
+# regular_sidebar()
 #formatting
 offset = 20
 st.set_page_config(layout="wide", page_title="Diccionario Completo")
