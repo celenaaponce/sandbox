e33521954cc8fda53d4cba2b9d1601244d063c0e
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-11 16:34:05 -0700

    Update Entrar.py

--- pages/Entrar.py
+++ pages/Entrar.py
@@ -7,8 +7,10 @@ import streamlit as st
 from streamlit import session_state as ss
 from modules.nav import MenuButtons
 from pages.account import get_roles
+
 if 'password_correct' not in ss:
     ss.password_correct = False
+st.write(st.session_state)
 st.markdown("""
     <style>
         div[data-testid="stSidebarNav"]{
