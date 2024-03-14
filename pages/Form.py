c09997f5d644f1615be7e02a8331b7e9c7ba2ffb
celenaaponce <138083273+celenaaponce@users.noreply.github.com>
2024-03-14 11:30:37 -0700

    Update Form.py

--- pages/Form.py
+++ pages/Form.py
@@ -25,12 +25,8 @@ from modules.nav import MenuButtons
 from pages.account import get_roles
 
 
-# If user refreshes the page, go to the login page because
-# in there we have the facility to check the login status.
-if 'authentication_status' not in ss:
-    st.switch_page('./pages/account.py')
 
-MenuButtons(get_roles())
+MenuButtons('')
 # show_pages(
 # [
 #     Page("Pagina_Principal.py", "Pagina Principal"),
